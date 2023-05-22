3bfd171930fc38fb3c0f106b275547a3503ddc85
jeraldmathewsjomy <134275453+jeraldmathewsjomy@users.noreply.github.com>
2023-05-22 15:09:22 +0530

    Add files via upload
    practice on css and html with javascript

---
+++ script.js
@@ -0,0 +1,92 @@
+/*var a=10;
+var b=20;
+var c=a+b;
+console.log("Addition");
+console.log(c);
+console.log("Subtraction");
+console.log(a-b);
+console.log("Multiplication");
+console.log(a*b);
+console.log("Division");
+console.log(a/b);
+console.log("postfix");
+console.log(a++);
+console.log("Prefix");
+console.log(++a);
+
+if (a<b) {
+
+    console.log("True");
+}
+
+else {
+
+    console.log("Faldse");
+}
+
+let person={
+
+    name:"jerald",
+    age:"15",
+    class:"s4cseb",
+};
+
+console.log(person.age)
+
+
+let fruits =["Apple","Grapes","Watermelon",11];
+fruits.pop();
+fruits.push("Banana");
+fruits.shift();
+fruits.unshift("Apple");
+console.log(fruits);
+
+let classes = [{
+
+    name:"Jerald",
+    age:20,
+    place:"penoville road",
+},
+
+{name:"Jacob",
+age:14,
+place:"Euston road",}];
+
+console.log(classes[0].name); 
+
+function mull(a,b) {
+    console.log(a*b);
+
+};
+mull(2,3); 
+
+const add1 =(a,b)=> {
+        console.log(a+b)
+
+}
+add1(1,2);
+
+
+let a=20;
+let b=10;
+
+if(a>b) {
+
+    console.log("True");
+}
+
+else {
+
+    console.log("False");
+} 
+
+for(i=1;i<=5;i++) {
+    console.log(i);
+} 
+
+i=1;
+while(i<=5) {
+    console.log(i);
+    i++;
+} */
+
